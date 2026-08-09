cfee71db63aa35164a3e8ab2f73496ba393ed015
David P. Sicilia <dpsicilia@gmail.com>
2026-08-09 16:20:17 -0400

    Some changes to the db interface.

--- src/test.lua
+++ src/test.lua
@@ -16,15 +16,22 @@ local function fmt_table( tbl )
   } )
 end
 
-local function print_table( tbl ) print( fmt_table( tbl ) ) end
+-- local function print_table( tbl ) print( fmt_table( tbl ) ) end
 
-local make_keybuilder
+-- local make_keybuilder
 
-local function make_hash_mt( cxn, key )
+local function make_hash_mt( cxn, key, getall )
   return {
+    __index=getall,
+    __pairs=function( _ ) return pairs( getall ) end,
+    __ipairs=function( _ ) return ipairs( getall ) end,
     __newindex=function( tbl, ht_key, ht_val )
-      cxn:hset( key, ht_key, ht_val )
       rawset( tbl, ht_key, ht_val )
+      if ht_val == nil then
+        cxn:hdel( key, ht_key )
+        return
+      end
+      cxn:hset( key, ht_key, ht_val )
     end,
   }
 end
@@ -32,12 +39,13 @@ end
 local function get_key( cxn, key )
   -- We're actually ending the chain and querying.
   if not cxn:exists( key ) then
-    error( 'failed to get non-existent key ' .. key )
+    -- error( 'failed to get non-existent key ' .. key )
+    return nil
   end
   local t = assert( cxn:type( key ) )
   if t == 'hash' then
-    local hash_mt = make_hash_mt( cxn, key )
-    return setmetatable( cxn:hgetall( key ), hash_mt )
+    local hash_mt = make_hash_mt( cxn, key, cxn:hgetall( key ) )
+    return setmetatable( {}, hash_mt )
   end
   if t == 'string' then return cxn:get( key ) end
   error( 'unrecognized key type ' .. t .. ' for key=' .. key )
@@ -45,11 +53,7 @@ end
 
 local function set_key( cxn, key, val )
   local t = type( val )
-  if t == 'string' then
-    cxn:set( key, key, val )
-    return
-  end
-  if t == 'number' then
+  if t == 'string' or t == 'number' then
     cxn:set( key, val )
     return
   end
@@ -63,39 +67,50 @@ local function set_key( cxn, key, val )
   error( 'unhandled lua value type ' .. t )
 end
 
-local keybuilder_mt = {
-  __index=function( tbl, next )
-    local cxn = assert( rawget( tbl, 'cxn' ) )
-    local key = rawget( tbl, 'key' )
-    return make_keybuilder( cxn, key, next )
-  end,
-  __newindex=function( tbl, next, val )
-    local cxn = assert( rawget( tbl, 'cxn' ) )
-    local key = rawget( tbl, 'key' )
-    set_key( cxn, key .. ':' .. next, val )
-  end,
-  __tostring=function( tbl )
-    local key = rawget( tbl, 'key' )
-    return '<key builder: key=' .. key .. '>'
-  end,
-}
-
-function make_keybuilder( cxn, start, next )
-  assert( start )
-  assert( type( start ) == 'string' )
-  local o = {}
-  o.cxn = cxn
-  o.key = start
-  if next then o.key = o.key .. ':' .. next end
-  if cxn:exists( o.key ) then return get_key( cxn, o.key ) end
-  return setmetatable( o, keybuilder_mt )
+local function del_key( cxn, key )
+  local t = type( key )
+  if t == 'string' or t == 'number' then
+    cxn:del( key )
+    return
+  end
+  error( 'unhandled lua value type ' .. t )
 end
 
+-- local keybuilder_mt = {
+--   __index=function( tbl, next )
+--     local cxn = assert( rawget( tbl, 'cxn' ) )
+--     local key = rawget( tbl, 'key' )
+--     return make_keybuilder( cxn, key, next )
+--   end,
+--   __newindex=function( tbl, next, val )
+--     local cxn = assert( rawget( tbl, 'cxn' ) )
+--     local key = rawget( tbl, 'key' )
+--     set_key( cxn, key .. ':' .. next, val )
+--   end,
+--   __tostring=function( tbl )
+--     local key = rawget( tbl, 'key' )
+--     return '<key builder: key=' .. key .. '>'
+--   end,
+-- }
+--
+-- function make_keybuilder( cxn, start, next )
+--   assert( start )
+--   assert( type( start ) == 'string' )
+--   local o = {}
+--   o.cxn = cxn
+--   o.key = start
+--   if next then o.key = o.key .. ':' .. next end
+--   if cxn:exists( o.key ) then return get_key( cxn, o.key ) end
+--   return setmetatable( o, keybuilder_mt )
+-- end
+
 local db_mt = {
-  __index=function( tbl, key )
-    return make_keybuilder( tbl.cxn, key )
-  end,
+  __index=function( tbl, key ) return get_key( tbl.cxn, key ) end,
   __newindex=function( tbl, key, val )
+    if val == nil then
+      del_key( tbl.cxn, key )
+      return
+    end
     set_key( tbl.cxn, key, val )
   end,
   __tostring=function( tbl )
@@ -122,32 +137,44 @@ local desc = { column_names={ 'value' }, row_names={}, data={} }
 
 local function add( path, value )
   insert( desc.row_names, path )
-  insert( desc.data, { value } )
+  insert( desc.data, { tostring( value ) } )
 end
 
-db.one.two.three = 555
-db.eight.nine = { foo='bar' }
-db.eight.nine.baz = 777
+db['one:two:three'] = 555
+db['eight:nine'] = { foo='bar' }
+db['eight:nine'].baz = 777
+db['eight:nine'].bam = 'world'
+
+assert( not db.zzz )
+assert( not db['one:zzz'] )
+assert( not db['eight:nine'].zzz )
 
 add( 'db', db )
-add( 'db.residents', db.residents )
-add( 'db.residents.david', fmt_table( db.residents.david ) )
-add( 'db.residents.david.city', db.residents.david.city )
+add( 'db["residents"]', db['residents'] )
+add( 'db["residents:david"]', fmt_table( db['residents:david'] ) )
+add( 'db["residents:david"].city', db['residents:david'].city )
 add( 'db.aaa', db.aaa )
-add( 'db.aaa.bbb', db.aaa.bbb )
-add( 'db.aaa.bbb.ccc', db.aaa.bbb.ccc )
-add( 'db.aaa.bbb.ccc.ddd', fmt_table( db.aaa.bbb.ccc.ddd ) )
-add( 'db.aaa.bbb.ccc.ddd.n', db.aaa.bbb.ccc.ddd.n )
+add( 'db["aaa:bbb"]', db['aaa:bbb'] )
+add( 'db["aaa:bbb:ccc"]', db['aaa:bbb:ccc'] )
+add( 'db["aaa:bbb:ccc:ddd"]', fmt_table( db['aaa:bbb:ccc:ddd'] ) )
+add( 'db["aaa:bbb:ccc:ddd:n"]', db['aaa:bbb:ccc:ddd:n'] )
+add( 'db["aaa:bbb:ccc:ddd"].n', db['aaa:bbb:ccc:ddd'].n )
 add( 'db.age', db.age )
-add( 'db["name:first"]', db.name.first )
-add( 'db["name:last"]', db.name.last )
-add( 'db.one.two.three', db.one.two.three )
-assert( not db.one.two.three['four'] )
-add( 'db.eight.nine', fmt_table( db.eight.nine ) )
-add( 'db.eight.nine.baz', db.eight.nine.baz )
-db.eight.nine.biff = 'hello'
-add( 'db.eight.nine', fmt_table( db.eight.nine ) )
-add( 'db.eight.nine.biff', db.eight.nine.biff )
+add( 'db["name:first"]', db['name:first'] )
+add( 'db["name:last"]', db['name:last'] )
+add( 'db["one:two:three"]', db['one:two:three'] )
+assert( not db['one:two:three']['four'] )
+db['eight:nine'].biff = nil
+add( 'db["eight:nine"]', fmt_table( db['eight:nine'] ) )
+add( 'db["eight:nine"].baz', db['eight:nine'].baz )
+add( 'db["eight:nine"].bam', db['eight:nine'].bam )
+db['eight:nine'].biff = 'hello'
+add( 'db["eight:nine"]', fmt_table( db['eight:nine'] ) )
+add( 'db["eight:nine"].biff', db['eight:nine'].biff )
+assert( db['eight:nine'] )
+db['eight:nine'] = nil
+assert( not db['eight:nine'] )
+add( 'db["eight:nine"]', tostring( db['eight:nine'] ) )
 
 for _, row in ipairs( printer.format_data_table_rows( desc ) ) do
   print( row )
