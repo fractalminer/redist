7eea9c1a75d791f46cc8399100ff466a9a95667a
David P. Sicilia <dpsicilia@gmail.com>
2026-09-06 14:53:45 -0400

    zstd->zlib

--- src/config.lua
+++ src/config.lua
@@ -10,7 +10,7 @@ return harden{
     -- HOST='127.0.0.1', -- loopback
     PORT=6379, --
     CONNECT_TIMEOUT_SECS=10,
-    COMPRESSION_METHOD='zstd',
+    COMPRESSION_METHOD='zlib',
     COMPRESSION_LEVEL=9,
   },
 
