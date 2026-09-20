0b52466a5aed062ce626a771646cc616197d65a5
David P. Sicilia <dpsicilia@gmail.com>
2026-09-20 17:27:22 -0400

    Restore redis host

--- src/config.lua
+++ src/config.lua
@@ -29,8 +29,8 @@ end
 -----------------------------------------------------------------
 return harden{
   general={
-    -- HOST='192.168.1.214', -- thelio/ethernet
-    HOST='127.0.0.1',
+    HOST='192.168.1.214', -- thelio/ethernet
+    -- HOST='127.0.0.1',
     PORT=6379,
     CONNECT_TIMEOUT_SECS=10,
     COMPRESSION_METHOD='zstd',
