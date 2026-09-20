6b26709d59f35ff91d2cc3e634802fa1a90b8d8f
David P. Sicilia <dpsicilia@gmail.com>
2026-09-20 17:13:06 -0400

    Update test.lua

--- src/test.lua
+++ src/test.lua
@@ -1,80 +1,25 @@
-local process_pool = require( 'process-pool' )
 local logger = require( 'moon.logger' )
-local time = require( 'moon.time' )
 
-local signal = require( 'posix.signal' )
+logger.level = logger.levels.DEBUG
 
-local debug = assert( logger.debug )
-local err = assert( logger.err )
+local lcache = require 'lcache'
+local lc<close> = lcache.open()
 
-local ProcessPool = assert( process_pool.ProcessPool )
+lc:blob_set( 'hello' )
+lc:blob_set( 'world' )
 
-local cmd = { 'bash', 'test.sh', 'hello' }
+-- for row in lc.db:nrows( 'SELECT * from blob' ) do
+--   print( table.unpack( row ) )
+-- end
 
-local stop = false
+print( 1, lc:blob_get( 'jjj' ) )
+print( 2, lc:blob_get( '47f3450b588f14654aa791b4abe4726f' ) )
+print( 3, lc:blob_get( '6119af7840b4bcb774ec6c6e1737b3a5' ) )
 
-logger.level = logger.levels.INFO
+lc:evict()
+lc:evict()
+lc:evict()
 
-signal.signal( signal.SIGINT, function() stop = true end )
-signal.signal( signal.SIGTERM, function() stop = true end )
-
-local function hold( pool, n )
-  pool:set( n )
-  while not stop do
-    debug( '[%d] running', pool:running_count() )
-    pool:log_pids()
-    pool:advance()
-    time.sleep( 1 )
-  end
-end
-
-local function ramp( pool )
-  local peak = 10
-  local increasing = true
-  local update_iters = 10
-  local iters = 0
-  while not stop do
-    debug( '[%d] running', pool:running_count() )
-    pool:log_pids()
-    pool:advance()
-    time.sleep( .3 )
-    iters = iters + 1
-    if iters >= update_iters then
-      iters = 0
-      if pool:running_count() > peak then increasing = false end
-      if pool:running_count() == 0 then increasing = true end
-      if increasing then
-        debug( '[%d] adding a process...', pool:target() )
-        pool:inc( 2 )
-      else
-        debug( '[%d] removing a process...', pool:target() )
-        pool:dec( 2 )
-      end
-    end
-  end
-end
-
-local function main()
-  do
-    local pool<close> = ProcessPool{ cmd=cmd }
-    stop = false
-    ramp( pool )
-  end
-  do
-    local pool<close> = ProcessPool{ cmd=cmd }
-    stop = false
-    hold( pool, 1 )
-  end
-  do
-    local pool<close> = ProcessPool{ cmd=cmd }
-    stop = false
-    hold( pool, 10 )
-  end
-end
-
-local ok, msg = pcall( main )
-if not ok then
-  err( msg )
-else
-  print( 'finished.' )
-end
+print( 1, lc:blob_get( 'jjj' ) )
+print( 2, lc:blob_get( '47f3450b588f14654aa791b4abe4726f' ) )
+print( 3, lc:blob_get( '6119af7840b4bcb774ec6c6e1737b3a5' ) )
